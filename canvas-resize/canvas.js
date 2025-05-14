fc2082ebde38313c5f590a4aca8db2deebe5fed4
Fasakin Henry <fasakinhenry@gmail.com>
2025-05-14 12:30:31 +0100

    feat: draw lines and circles

--- canvas-resize/canvas.js
+++ canvas-resize/canvas.js
@@ -8,6 +8,23 @@ canvas.height = innerHeight;
 const ctx = canvas.getContext('2d');
 
 // Draw rectangles at different positions on the screen
+ctx.fillStyle = 'rgba(255, 0, 0, 0.7)';
 ctx.fillRect(100, 100, 100, 100);
+ctx.fillStyle = 'rgba(0, 0, 255, 0.7)';
 ctx.fillRect(300, 300, 100, 100);
+ctx.fillStyle = 'rgba(0, 255, 0, 0.7)';
 ctx.fillRect(400, 100, 100, 100);
+
+// Draw lines
+ctx.beginPath();
+ctx.moveTo(100, 300);
+ctx.lineTo(300, 100);
+ctx.lineTo(400, 300);
+ctx.strokeStyle = 'rgba(0, 0, 255, 0.7)';
+ctx.stroke();
+
+// Arc / Circle
+ctx.beginPath();
+ctx.arc(300, 300, 30, 0, Math.PI * 2);
+ctx.strokeStyle = 'blue';
+ctx.stroke();
