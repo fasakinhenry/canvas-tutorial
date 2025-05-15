b2cedaaa070d4d7df4c4170342475d55c02db97d
Fasakin Henry <fasakinhenry@gmail.com>
2025-05-15 10:42:05 +0100

    feat: animate circle properly

--- canvas-resize/circle-animate.js
+++ canvas-resize/circle-animate.js
@@ -7,13 +7,22 @@ canvas.width = innerWidth;
 // Create the context for drawing on canvas
 const c = canvas.getContext('2d');
 
+// create initial values for the circle properties
+let x = 200;
+let y = 200;
+let dx = 5;
+let dy = 4;
+
 function animate() {
   requestAnimationFrame(animate);
+  c.clearRect(0, 0, innerWidth, innerHeight);
   // Draw a circle
   c.beginPath();
-  c.arc(200, 200, 30, 0, Math.PI * 2);
+  c.arc(x, y, 30, 0, Math.PI * 2);
   c.strokeStyle = 'blue';
   c.stroke();
+  x += dx;
+  y += dy;
 }
 
 animate();
