e846a584986d77798f7263e9c09dd2d9fdc5ee00
Fasakin Henry <fasakinhenry@gmail.com>
2025-05-15 11:01:32 +0100

    feat(OOP): create a class for circle

--- canvas-resize/circle-animate.js
+++ canvas-resize/circle-animate.js
@@ -7,6 +7,31 @@ canvas.width = innerWidth;
 // Create the context for drawing on canvas
 const c = canvas.getContext('2d');
 
+// Create the circle class
+class Circle {
+  constructor(x, y, radius, dx, dy) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.dx = dx;
+    this.dy = dy;
+  }
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    c.fill();
+  }
+  update() {
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0)
+      this.dx = -this.dx;
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0)
+      this.dy = -this.dy;
+    this.x += this.dx;
+    this.y += this.dy;
+    this.draw();
+  }
+}
+
 // create initial values for the circle properties
 let x = Math.random() * innerWidth;
 let y = Math.random() * innerHeight;
