85af387402ef401fcf1f21923698b510d6ab01e4
Fasakin Henry <fasakinhenry@gmail.com>
2025-05-15 10:46:37 +0100

    feat: animate the circle horizontally

--- canvas-resize/circle-animate.js
+++ canvas-resize/circle-animate.js
@@ -10,6 +10,7 @@ const c = canvas.getContext('2d');
 // create initial values for the circle properties
 let x = 200;
 let y = 200;
+let radius = 30;
 let dx = 5;
 let dy = 4;
 
@@ -18,11 +19,12 @@ function animate() {
   c.clearRect(0, 0, innerWidth, innerHeight);
   // Draw a circle
   c.beginPath();
-  c.arc(x, y, 30, 0, Math.PI * 2);
+  c.arc(x, y, radius, 0, Math.PI * 2);
   c.strokeStyle = 'blue';
   c.stroke();
+  if (x + radius > innerWidth || x - radius < 0) dx = -dx;
   x += dx;
-  y += dy;
+//   y += dy;
 }
 
 animate();
